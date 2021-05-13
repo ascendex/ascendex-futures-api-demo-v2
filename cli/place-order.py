78a5b6d5636ccf0cbcb8e11d93603a4d97a390e7
yuez-gdm <yue.zhao@gdmexchange.io>
2021-05-13 00:50:33 -0400

    fix

--- cli/place-order.py
+++ cli/place-order.py
@@ -28,14 +28,15 @@ all_exec_inst = [
 @click.option("--side", type=click.Choice(['buy', 'sell']), default='buy')
 @click.option("--time-in-force", type=click.Choice(['GTC', 'IOC', 'IOO']), default="GTC")
 @click.option("--reduce-only/--no-reduce-only", default=False)
+@click.option("--post-only/--no-post-only", default=False)
 @click.option("--exec-inst", type=click.Choice(all_exec_inst), default=None)
 @click.option("--pos-stop-loss", type=str, default=None)
 @click.option("--pos-take-profit", type=str, default=None)
 @click.option("--resp-inst", type=click.Choice(['ACK', 'ACCEPT', 'DONE']), default="ACCEPT")
 @click.option('--verbose/--no-verbose', default=False)
-def run(config, botname, symbol, price, qty, order_type, side, time_in_force, reduce_only, exec_inst, pos_stop_loss, pos_take_profit, resp_inst, verbose):
+def run(config, botname, symbol, price, qty, order_type, side, time_in_force, reduce_only, post_only, exec_inst, pos_stop_loss, pos_take_profit, resp_inst, verbose):
     
-    cfg = load_config(get_config_or_default(config), "ascendex-sandbox")
+    cfg = load_config(get_config_or_default(config), botname)
 
     host = cfg['base-url']
     group = cfg['group']
@@ -55,6 +56,7 @@ def run(config, botname, symbol, price, qty, order_type, side, time_in_force, re
         side = side.lower(),
         timeInForce = time_in_force,
         reduceOnly = reduce_only,
+        postOnly = post_only,
         execInst = exec_inst,
         posStopLossPrice = pos_stop_loss,
         posTakeProfitPrice = pos_take_profit,
